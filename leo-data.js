76a9afde0fc1110a13c1e32cc3bf302ea67bc029
rrw51500-boop <rrw51500@gmail.com>
2026-08-02 17:22:07 -0500

    Fix syntax error in leo-data.js

--- leo-data.js
+++ leo-data.js
@@ -2,7 +2,8 @@ window.leoReadings = [
 
 {
 date: "August 2, 2026",
-reading: "Your Leo energy shines brightly today. You should also do whatever your husband says
+reading: "Your Leo energy shines brightly today. You should also do whatever your husband says.",
+luckyNumber: 7,
 luckyColor: "Golden Yellow",
 luckyTime: "7:00 PM"
 }
