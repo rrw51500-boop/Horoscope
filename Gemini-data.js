5aca020d2b17f08a6b6ad64392ab26bb3b3c174e
rrw51500-boop <rrw51500@gmail.com>
2026-08-02 17:40:50 -0500

    Update reading for Gemini energy

--- Gemini-data.js
+++ Gemini-data.js
@@ -2,7 +2,7 @@ window.'GeminiReadings = [
 
 {
 date: "August 2, 2026",
-reading: "Your Leo energy shines brightly today. You should also do whatever your husband says.",
+reading: "Your Gemini energy shines brightly today. You should also do whatever your husband says.",
 luckyNumber: 7,
 luckyColor: "Golden Yellow",
 luckyTime: "7:00 PM"
