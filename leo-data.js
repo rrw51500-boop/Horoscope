da59dfb6946f6d646e09a57f96e12e17df7c94c8
rrw51500-boop <rrw51500@gmail.com>
2026-08-02 16:59:05 -0500

    Update Leo reading date to August 2, 2026

--- leo-data.js
+++ leo-data.js
@@ -1,7 +1,7 @@
 window.leoReadings = [
 
 {
-date: "July 28, 2026",
+date: "August 2, 2026",
 reading: "Your Leo energy shines brightly today. You should also do whatever your husband says
 luckyColor: "Golden Yellow",
 luckyTime: "7:00 PM"
